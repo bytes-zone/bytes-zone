a31719d20dab3fb8c8f2f9b3091cfc97135aa556
Brian Hicks <brian@brianthicks.com>
2020-11-27 13:55:37 -0600

    compress CSS and HTML

--- default.nix
+++ default.nix
@@ -1,15 +1,28 @@
 { ... }:
 let
   sources = import ./nix/sources.nix;
-  nixpkgs = import sources.nixpkgs { };
+  pkgs = import sources.nixpkgs { };
   gitignore = import sources.gitignore { };
-in nixpkgs.stdenv.mkDerivation {
+in pkgs.stdenv.mkDerivation {
   name = "bytes.zone";
   src = gitignore.gitignoreSource ./.;
 
-  buildInputs = [ nixpkgs.zola ];
+  buildInputs =
+    [ pkgs.zola pkgs.yuicompressor pkgs.nodePackages.html-minifier ];
   buildPhase = ''
     zola build
+
+    echo "compressing CSS"
+    find public \
+      -type f -name '*.css' \
+      -exec yuicompressor -o {}.min {} \; \
+      -exec mv {}.min {} \;
+
+    echo "compressing HTML"
+    find public \
+      -type f -name '*.html' \
+      -exec html-minifier --collapse-whitespace --decode-entities --remove-comments --remove-attribute-quotes --remove-redundant-attributes --remove-optional-tags --remove-script-type-attributes --remove-style-link-type-attributes -o {}.min {} \; \
+      -exec mv {}.min {} \;
   '';
 
   installPhase = ''
