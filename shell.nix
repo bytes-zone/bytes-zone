3248802a0a9a9fe843eba87e6d95e5ef8ef3b5f5
Brian Hicks <brian@brianthicks.com>
2020-11-28 06:05:53 -0600

    add pa11y for checking the site as I build it

--- shell.nix
+++ shell.nix
@@ -3,8 +3,9 @@ let
   sources = import ./nix/sources.nix;
   nixpkgs = import sources.nixpkgs { };
   niv = import sources.niv { };
+  pa11y = import ./nix/pkgs/pa11y { pkgs = nixpkgs; };
 in with nixpkgs;
 stdenv.mkDerivation {
   name = "bytes.zone";
-  buildInputs = [ niv.niv git zola ];
+  buildInputs = [ niv.niv git zola pa11y.pa11y ];
 }
