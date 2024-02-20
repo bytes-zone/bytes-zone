432ab434eedc61ff3c683236d6b9062559e3c830
Brian Hicks <brian@brianthicks.com>
2024-02-20 16:29:51 -0600

    ignore woff2 files in bytes-zone-public

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         packages.bytes-zone-public = pkgs.stdenv.mkDerivation {
           name = "bytes.zone-public";
           src = builtins.filterSource
-            (path: type: builtins.match ".+(png|css|js)$" path == null) ./.;
+            (path: type: builtins.match ".+(png|css|js|woff2)$" path == null) ./.;
 
           buildInputs = [ pkgs.zola pkgs.nodePackages.html-minifier ];
           buildPhase = ''
